3bc90a967c6fc5a74db9c3b4f45592e70925410c
jodeikamartynas <martynas.jodeika@ktu.edu>
2021-07-04 17:19:34 +0300

    name change and runelite version update

--- src/main/java/com/entitycapture/EntityCaptureConfig.java
+++ src/main/java/com/entitycapture/EntityCaptureConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.entitycapture;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("example")
-public interface ExampleConfig extends Config
+public interface EntityCaptureConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
